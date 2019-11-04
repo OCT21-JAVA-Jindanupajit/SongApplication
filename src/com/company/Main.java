6f3adf6b6732e44927df942d36a1e5ce24c3c9c2
Krissada Jindanupajit <jindanupajit@gmail.com>
2019-11-04 14:25:57 -0500

    Finish

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,30 +1,39 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<XXXX> songs = new ArrayList<>();
+        ArrayList<Song> songs = new ArrayList<>();
+
+        Scanner kb = new Scanner(System.in);
+
         for (int i = 0; i< 3;i++){
-            String artist = "Prince";//modify program to prompt user for an artist
-            String title = "Purple Rain"; //modify program to prompt user for a title
-            XXXX s = new Song();
+            System.out.printf("Record #%d: \n", i+1);
+
+            System.out.printf( "artist > ");
+            String artist = kb.nextLine();//modify program to prompt user for an artist
+
+            System.out.printf( "title > ");
+            String title = kb.nextLine(); //modify program to prompt user for a title
+            Song s = new Song();
             s.setArtist(artist);
             s.setTitle(title);
             songs.add(s);
         }
 
         //print all the songs in the array list
-        for(Song xxxx: songs){
-            System.out.println(xxxx.display());
+        for(Song song: songs){
+            System.out.println(song.display());
         }
 
         //look for blue and print if found
         String titleToFind = "Purple Rain";
-        for (XXXX xxxx: xxxx){
-            if (xxxx.getTitle().equals(titleToFind)){
+        for (Song song: songs){
+            if (song.getTitle().equals(titleToFind)){
                 System.out.printf("I found %s \r\n",song.display());
             }
         }
